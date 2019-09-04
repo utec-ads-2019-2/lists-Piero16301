f9c2fa5b0f9f45aa1d31d72b4a13b674aa526ee5
Piero Angelo <piero.morales@utec.edu.pe>
2019-09-04 18:47:10 -0500

    ..

---
+++ cmake-build-debug/CMakeFiles/Listas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Listas.dir/main.cpp.obj"
+  "Listas.pdb"
+  "Listas.exe"
+  "Listas.exe.manifest"
+  "libListas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Listas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
